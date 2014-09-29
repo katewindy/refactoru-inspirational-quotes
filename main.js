c2c90b11c8ade0f0e8d35b274134fdae2224b08a
Kate Windheuser <roboticbrain@gmail.com>
2014-09-29 17:31:38 -0600

    set most of the styling

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 $(document).on('ready', function() {
-  
+  	$('.form').hide();
+
+  	$('#addQuote').on('click', function(){
+  		$(this).hide();
+  		$('.form').slideDown('slow');
+  		$('#editname').val($('#name').text());
+		$('#editbio').val($('#bio').text());
+
+  	});
+
+  	$('#saveQuote').on('click', function(){
+  		$('.form').slideUp('slow');
+  		$('#addQuote').show();
+  		$('#name').text($('#editname').val());
+		$('#bio').text($('#editbio').val());
+  	});
 });
+
